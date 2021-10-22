56fa766f05a561cffc7782e7d13686a7f5c99c99
Ademir Mazer Jr <ademir.mazer.jr@gmail.com>
2021-10-22 18:10:06 -0300

    testes user service

---
+++ src/integration-tests/users.test.js
@@ -0,0 +1,33 @@
+const User = require('../api/users/user');
+const { expect } = require('chai');
+
+const mongoDbUrl = 'mongodb://localhost:27017/Cookmaster';
+const url = 'http://localhost:3000';
+
+describe('INT-01. User service', () => {
+
+    it('Inserir usuário no banco usando o service User', async () => {
+        const user = new User();
+
+        const res = await user.create('Nuno', 'nuno@gg.com', '123');
+
+        expect(user.role).equal('user');
+    });
+
+    it('Inserir usuário admin no banco usando o service User', async () => {
+        const user = new User();
+
+        const res = await user.createAdmin('Nuno', 'nuno@gg.com', '123');
+
+        expect(user.role).equal('admin');
+    });
+
+    it('Verificar que email já existe', async () => {
+        const user = new User();
+
+        const res = await user.createAdmin('Nuno', 'nuno@gg.com', '123');
+
+        expect(await user.emailExists(user.email)).equal(true);
+    });
+
+});
